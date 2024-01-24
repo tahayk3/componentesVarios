f0a7215fa09a7b2257db9496c4db628bce222bcb
FamilabXela <familabxela@gmail.com>
2024-01-24 15:25:50 -0600

    Agregando modal(sin css) para el formulario, utilizando un useEffect y la funcion asíncrona setTimeOut

--- src/components/FormularioComponent.jsx
+++ src/components/FormularioComponent.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "../hooks/useForm";
+import  Modal  from "./Modal"; 
 
 export const FormularioComponent = () =>{
-    
+    const [mostrarMensaje, setMostrarMensaje] = useState(false);
+
     const initialForm = {
         userName:"",
         email: "",
@@ -16,41 +18,64 @@ export const FormularioComponent = () =>{
     const onSubmit = (event) =>{
         event.preventDefault();
         console.log(formState);
+        setMostrarMensaje(true);
     }
 
+
+
+    //Este useEfect cambia una variable de estado, luego de 3 segundos
+    useEffect(()=>{
+        if(mostrarMensaje){
+         const timeOutMessage = setTimeout(() => {
+             setMostrarMensaje(false);    
+         }, 3000);
+ 
+         return () =>{
+             clearTimeout(timeOutMessage);
+         };
+ 
+        }
+     },[mostrarMensaje]);
+
+
     return(
-        <form onSubmit={onSubmit}>
-            <div className="form-group">
-                <label htmlFor="userName">User name</label>
-                <input 
-                    type="text" 
-                    className="form-control" 
-                    name="userName" 
-                    value={userName}
-                    onChange={onInputChange}
-                    placeholder="Enter user name"/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="email">Email address</label>
-                <input 
-                    type="email" 
-                    className="form-control" 
-                    name="email"
-                    value= {email}
-                    onChange={onInputChange}
-                    placeholder="Enter email"/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input 
-                    type="password" 
-                    className="form-control" 
-                    name="password"
-                    value= {password}
-                    onChange={onInputChange}
-                    placeholder="Password"/>
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+        <>
+            <form onSubmit={onSubmit} >
+            <Modal
+                mostrarMensaje={mostrarMensaje}
+            />
+                <div className="form-group">
+                    <label htmlFor="userName">User name</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        name="userName" 
+                        value={userName}
+                        onChange={onInputChange}
+                        placeholder="Enter user name"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email address</label>
+                    <input 
+                        type="email" 
+                        className="form-control" 
+                        name="email"
+                        value= {email}
+                        onChange={onInputChange}
+                        placeholder="Enter email"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="password" 
+                        className="form-control" 
+                        name="password"
+                        value= {password}
+                        onChange={onInputChange}
+                        placeholder="Password"/>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </>
     );
 }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Modal({mostrarMensaje}){
+    return(
+      <>
+      {mostrarMensaje && <h1>ENVIADOOOOOOOOOOOOOOOOOOOOOOOO</h1> }
+      </>
+    );
+}
+
+export default Modal;
